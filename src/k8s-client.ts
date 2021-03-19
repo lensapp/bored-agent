800517d3a03284ce8398011af2c5cc91e9271854
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2021-03-19 17:35:19 +0200

    Replace kubernetes-client with got (#11)
    * replace kubernetes-client with got
    
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>
    
    * fix tests
    
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>
    
    * refactor
    
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

---
+++ src/k8s-client.ts
@@ -0,0 +1,79 @@
+import got, { Headers } from "got";
+import * as fs from "fs/promises";
+
+export const kubernetesHost = process.env.KUBERNETES_HOST || "kubernetes.default.svc";
+export const caCert = process.env.CA_CERT || "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt";
+export const serviceAccountTokenPath = process.env.SERVICEACCOUNT_TOKEN_PATH || "/var/run/secrets/kubernetes.io/serviceaccount/token";
+
+export type ErrorResponse = {
+  status: number,
+  code: number,
+  reason: string,
+  message: string
+};
+
+export class K8sError extends Error {
+  response: ErrorResponse;
+
+  constructor(errorResponse: ErrorResponse) {
+    super(errorResponse.message);
+    this.response = errorResponse;
+  }
+}
+
+export class K8sClient {
+  private serviceAccountToken = "";
+  private caCert = "";
+  private headers: Headers = {};
+
+  async init() {
+    this.serviceAccountToken = (await fs.readFile(serviceAccountTokenPath)).toString();
+    this.caCert = (await fs.readFile(caCert)).toString();
+
+    this.headers = {
+      "Authorization": `Bearer ${this.serviceAccountToken}`,
+      "Accept": "application/json"
+    };
+  }
+
+  async get<T>(path: string, headers = {}): Promise<T> {
+    const response = await got.get(`https://${kubernetesHost}${path}`, {
+      headers: {
+        ...headers,
+        ...this.headers,
+      },
+      https: {
+        certificateAuthority: this.caCert
+      },
+      responseType: "json",
+      throwHttpErrors: false
+    });
+
+    if (response.statusCode !== 200) {
+      throw new K8sError(response.body as ErrorResponse);
+    }
+
+    return response.body as T;
+  }
+
+  async patch<T>(path: string, obj: Object, headers = {}) {
+    const response = await got.patch(`https://${kubernetesHost}${path}`, {
+      headers: {
+        ...this.headers,
+        ...headers
+      },
+      https: {
+        certificateAuthority: this.caCert
+      },
+      responseType: "json",
+      throwHttpErrors: false,
+      json: obj
+    });
+
+    if (response.statusCode !== 200) {
+      throw new K8sError(response.body as ErrorResponse);
+    }
+
+    return response.body as T;
+  }
+}

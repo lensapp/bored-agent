c4cea6b803f00685471916fbe4b81e42f22dbe4c
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2023-11-14 10:42:51 +0200

    fix: remove insecure mode if no ca is given (#257)
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/__tests__/agent-proxy.test.ts
+++ src/__tests__/agent-proxy.test.ts
@@ -1,18 +1,32 @@
 import { Agents } from "got/dist/source";
 import { HttpsProxyAgent } from "https-proxy-agent";
 import { AgentProxy } from "../agent-proxy";
+import { PassThrough } from "stream";
+import { Stream } from "bored-mplex";
 
 describe("AgentProxy", () => {
   const OLD_ENV = process.env;
   let agent: AgentProxy;
+  let connect: any;
 
   beforeEach(() => {
     jest.resetModules();
+    connect = jest.fn((_opts: any, callback: () => void) => {
+      process.nextTick(() => {
+        callback();
+      });
+
+      return new PassThrough();
+    });
     process.env = { ...OLD_ENV };
     agent = new AgentProxy({
       boredServer: "https://bored.acme.org",
       boredToken: "foo.bar.baz",
       idpPublicKey: "this-is-not-valid"
+    }, { 
+      tlsConnect: connect,
+      fileExists: jest.fn(() => false),
+      readFile: jest.fn(),
     });
   });
 
@@ -51,6 +65,80 @@ describe("AgentProxy", () => {
 
       expect(agent.buildGotOptions().agent).toBeDefined();
       expect((agent.buildGotOptions().agent as Agents).https).toBeInstanceOf(HttpsProxyAgent);
+    });
+  });
+
+  describe("handleDefaultRequestStream", () => {
+    it("opens TLS socket with correct options", () => {
+      const stream = new Stream(1, {} as any);
+
+      agent.handleDefaultRequestStream(stream);
+
+      expect(connect).toHaveBeenCalledWith(expect.objectContaining({
+        host: "kubernetes.default.svc",
+        port: 443,
+        timeout: 1800000,
+      }), expect.any(Function));
+    });
+
+    describe("given CA file exists", () => {
+      beforeEach(() => {
+        agent = new AgentProxy({
+          boredServer: "https://bored.acme.org",
+          boredToken: "foo.bar.baz",
+          idpPublicKey: "this-is-not-valid"
+        }, { 
+          tlsConnect: connect,
+          fileExists: jest.fn(() => true),
+          readFile: jest.fn(() => Buffer.from("fake-ca") as any),
+        });
+      });
+
+      it("passes CA to TLS socket", () => {
+        const stream = new Stream(1, {} as any);
+
+        agent.handleDefaultRequestStream(stream);
+
+        expect(connect).toHaveBeenCalledWith(expect.objectContaining({
+          host: "kubernetes.default.svc",
+          port: 443,
+          ca: expect.any(Buffer),
+        }), expect.any(Function));
+      });
+    });
+
+    describe("given CA file does not exist", () => {
+      beforeEach(() => {
+        agent = new AgentProxy({
+          boredServer: "https://bored.acme.org",
+          boredToken: "foo.bar.baz",
+          idpPublicKey: "this-is-not-valid"
+        }, { 
+          tlsConnect: connect,
+          fileExists: jest.fn(() => false),
+          readFile: jest.fn(),
+        });
+      });
+
+      it("does not pass CA to TLS socket", () => {
+        const stream = new Stream(1, {} as any);
+
+        agent.handleDefaultRequestStream(stream);
+
+        expect(connect).toHaveBeenCalledWith(expect.not.objectContaining({
+          ca: expect.anything(),
+        }), expect.any(Function));
+      });
+
+      it("does not configure 'reject unauthorized' to TLS socket", () => {
+        const stream = new Stream(1, {} as any);
+
+        agent.handleDefaultRequestStream(stream);
+
+        expect(connect).toHaveBeenCalledWith(expect.not.objectContaining({
+          rejectUnauthorized: false,
+        }), expect.any(Function));
+      });
     });
   });
 });

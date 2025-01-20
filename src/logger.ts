f7c696e04b20c179ae1470d705af24275df50680
Panu Horsmalahti <phorsmalahti@mirantis.com>
2025-01-20 17:04:22 +0200

    feat: configurable log level (#325)

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,14 @@
 import pino from "pino";
 
+export const logLevel = process.env.LOG_LEVEL || "info";
+
 const logger = pino({
+  level: logLevel,
   transport: {
     target: "pino-pretty",
     options: {
       ignore: "pid,hostname",
-      translateTime: true,
+      translateTime: true
     },
   },
 });
